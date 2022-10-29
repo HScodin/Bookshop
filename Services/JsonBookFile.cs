c36ffd0640bbc2dbeb6d4fe7e50255a18d92335b
Muhammad Hassan <muhammadhassanmicro10@hotmail.com>
2022-10-29 05:48:27 -0700

    Create Service for get record of books and configure in startup

---
+++ Services/JsonBookFile.cs
@@ -0,0 +1,35 @@
+﻿using Bookshop.Modals;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Bookshop.Services
+{
+    public class JsonBookFile
+    {
+        public JsonBookFile(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment;
+
+        public string jsonfilepath
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "books.json");
+            } 
+        }
+
+        public IEnumerable<Book> getBookRecord()
+        {
+            using (var json_file = File.OpenText(jsonfilepath))
+            return JsonSerializer.Deserialize<Book[]>(json_file.ReadToEnd());
+        }
+    }
+}
